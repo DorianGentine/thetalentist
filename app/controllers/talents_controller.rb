dc103f0a96606f5288acd4033d98ad2ec087d989
Donatien Rolland <donatien@rollandmail.com>
2018-05-21 18:19:46 +0200

    show routes

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -3,7 +3,8 @@ class TalentsController < ApplicationController
   def index
     @talents = Talent.all
   end
-  # def show
-  #   @talent = Talent.find(session[:talent_id])
-  # end
+  def show
+    @talent = Talent.find(params[:id])
+  end
+
 end
