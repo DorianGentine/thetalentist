4cae31b6e3e4e8dd506f4d6623f6cf34e76a7f10
Donatien Rolland <donatien@rollandmail.com>
2018-05-30 08:10:09 +0200

    routes modif

--- app/controllers/talents/sessions_controller.rb
+++ app/controllers/talents/sessions_controller.rb
@@ -9,22 +9,21 @@ class Talents::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  #   # raise
-  #   session[:talent_id] = @talent.id
-  #   redirect_to talent_path
-  # end
+  def create
+    super
+    talent_path(resource)
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
 end
