7e055472bc867d6d01102d1e037bd4cb0970c10e
Donatien Rolland <donatien@rollandmail.com>
2018-05-31 15:57:53 +0200

    js modal et ruby modal

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,29 +1,27 @@
-function revealModal(myModal,myBtn) {
-  // Get the modal
-  var modal = document.getElementById('myModal');
+function revealModal() {
+  const triggerModal = (event) => {
+    let targetModalID = event.currentTarget.dataset.target
+    let targetModal = document.getElementById(targetModalID)
+    const span = document.getElementsByClassName("close")[0];
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+    targetModal.style.display = "block";
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+      targetModal.style.display = "none";
+    }
 
-  // When the user clicks the button, open the modal
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
+    window.onclick = function(event) {
+      if (event.target == targetModal) {
+          targetModal.style.display = "none";
       }
+    }
   }
+
+  const modalButtonsList = document.querySelectorAll('.modal-button')
+  modalButtonsList.forEach((button) => {
+    button.addEventListener("click", triggerModal);
+  })
 }
+
 
 export { revealModal }
