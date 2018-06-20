6ca3e7c8e46bd6f732c3320646fb9e9ec0df8c6b
Donatien Rolland <donatien@rollandmail.com>
2018-06-20 12:25:47 +0200

    add and remove validated

--- db/migrate/20180619150817_add_validated_to_startups.rb
+++ db/migrate/20180619150817_add_validated_to_startups.rb
@@ -1,6 +1,5 @@
 class AddValidatedToStartups < ActiveRecord::Migration[5.2]
   def change
     add_column :startups, :validated, :boolean, null: false, default:false
-    add_column :startups, :valideted, :boolean
   end
 end
--- db/migrate/20180620091623_remove_valideted_and_validated_from_headhunter.rb
+++ db/migrate/20180620091623_remove_valideted_and_validated_from_headhunter.rb
@@ -1,6 +1,6 @@
 class RemoveValidetedAndValidatedFromHeadhunter < ActiveRecord::Migration[5.2]
   def change
-    remove_column :headhunters, :valideted, :boolean
+    # remove_column :headhunters, :valideted, :boolean
     remove_column :headhunters, :validated, :boolean
   end
 end
