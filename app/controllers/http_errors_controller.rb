89c04a69b2da8e8f2a984f3f7d1f6c8fe01a4dc1
Donatien Rolland <donatien@rollandmail.com>
2018-07-06 15:10:38 +0200

    new errors pages

--- app/controllers/http_errors_controller.rb
+++ app/controllers/http_errors_controller.rb
@@ -1,11 +1,17 @@
 class HttpErrorsController < ApplicationController
+
+  skip_after_action :verify_policy_scoped, :only => [:error_500, :error_404, :error_422 ]
+  skip_after_action :verify_authorized, :only => [:error_500, :error_404, :error_422 ]
+
   def error_404
     @not_found_path = params[:not_found]
   end
 
   def error_422
+    render(:status => 422)
   end
 
   def error_500
+    render(:status => 500)
   end
 end
