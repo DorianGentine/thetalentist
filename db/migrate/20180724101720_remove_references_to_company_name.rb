889315afdfbc9c715166b9f607ad6a6cf83c3f15
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2018-08-30 16:06:21 +0200

    first mail done

--- db/migrate/20180724101720_remove_references_to_company_name.rb
+++ db/migrate/20180724101720_remove_references_to_company_name.rb
@@ -1,5 +1,5 @@
 class RemoveReferencesToCompanyName < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :experiences, :company_name
+    # remove_reference :experiences, :company_name
   end
 end
