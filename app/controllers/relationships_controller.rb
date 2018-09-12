f7d1378f7095a983f7e72cdc6344cbb989c974b7
Donatien Rolland <donatien@rollandmail.com>
2018-09-12 12:35:39 +0200

    ajax done

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,28 +1,26 @@
 class RelationshipsController < ApplicationController
 
   def create
-    @relationship = Relationship.new
-
     @relationship = Relationship.create(relationship_params)
+    authorize @relationship
+
     @talent = @relationship.talent
     @relationship.status = "pending"
     if @relationship.save
-      headhunter = @relationship.headhunter
-      talent = @relationship.talent
-      headhunter.send_message(talent, "#{headhunter.firstname}, souhaite rentrer en contact avec vous", "#{headhunter.firstname}")
+      @headhunter = @relationship.headhunter
+      @headhunter.send_message(@talent, "#{@headhunter.firstname}, souhaite rentrer en contact avec vous", "#{@headhunter.firstname}")
       flash[:success] = "Relationship was created!"
-      redirect_to repertoire_path, flash: {notice: "Vous êtes maintenant en relation avec ce Talent."}
-      # respond_to do |format|
-      #   format.html { redirect_to repertoire_path }
-      #   format.js  # <-- will render `app/views/reviews/create.js.erb`
-      # end
+      # redirect_to repertoire_path, flash: {notice: "Vous êtes maintenant en relation avec ce Talent."}
+      respond_to do |format|
+        format.html { redirect_to repertoire_path }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
     else
       respond_to do |format|
         format.html { render 'repertoire' }
         format.js  # <-- idem
       end
     end
-    authorize @relationship
   end
 
   private
