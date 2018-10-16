a85f5f4c93d4eb357a71ba7c5e2375acb2616c39
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2018-10-16 16:52:59 +0700

    automatic character indicator

---
+++ app/javascript/components/limitedCharacterIndicator.js
@@ -0,0 +1,24 @@
+var limitedCharacter = document.getElementsByClassName("limited-character");
+
+if (limitedCharacter) {
+  for (var i = 0; i < limitedCharacter.length ; i++) {
+    var inputLength = limitedCharacter[i].value.length;
+    var limitCharacter = limitedCharacter[i].maxLength;
+    limitedCharacter[i].parentNode.insertAdjacentHTML("afterend",'<p id="aide-short_resume" class="titre-rubrique-1 float-right"></p>');
+    var aideShortResume = document.getElementById("aide-short_resume");
+
+    limitedCharacter[i].addEventListener("input", function(e) {
+      inputLength = e.target.value.length;
+      aideShortResume.textContent = inputLength + "/" + limitCharacter;
+    })
+
+    limitedCharacter[i].addEventListener("focus", function(e) {
+        aideShortResume.textContent = inputLength + "/" + limitCharacter;
+        aideShortResume.classList.remove("hidden");
+    })
+
+    limitedCharacter[i].addEventListener("blur", function(e) {
+      aideShortResume.classList.add("hidden");
+    })
+  }
+}
