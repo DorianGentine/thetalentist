1a28293195a232934058fc8aeedf570e145d059e
Donatien Rolland <donatien@rollandmail.com>
2018-10-18 17:04:03 +0700

    ok

--- app/javascript/components/datepicker.js
+++ app/javascript/components/datepicker.js
@@ -3,11 +3,18 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 // install yarn add flatpickr et suivre les infos https://flatpickr.js.org/
+function onMonthOrYearChange(dObj, dStr, fp) {
+  fp.setDate(new Date(fp.currentYear, fp.currentMonth));
+}
 
 if (data) {
 
   flatpickr(".datepicker1", {
-     dateFormat: "d M - Y",
-
+     dateFormat: "M - Y",
+     onYearChange: onMonthOrYearChange,
+    onMonthChange: onMonthOrYearChange
   });
 }
+
+
+
