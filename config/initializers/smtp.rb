af37ca0c13e66781b0174b425a54e86da7d8269a
Donatien Rolland <donatien@rollandmail.com>
2018-11-19 14:35:43 +0700

    beta

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@
 ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: 'thetalentist.com',
+  domain: 'betatalentist.com',
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :login,
