8383dad346abd8c994657f82c05777a16ded4c79
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2018-12-17 14:36:29 +0700

    verifDate

--- app/javascript/components/verifDate.js
+++ app/javascript/components/verifDate.js
@@ -1,23 +1,34 @@
 function verifDateWithMonthAndYear(){
 
-  const dates = document.querySelectorAll('.verifDateWithMonthAndYear')
+  var dateModules = document.querySelectorAll(".date_module")
   const date_regex = /^(0[1-9]|1\d|2\d|3[01])-(19|20)\d{2}$/ ;
-  if (dates.length > 0) {
-    dates.forEach((date) => {
-      date.addEventListener('change', function(){
-        if(!(date_regex.test(date.value))){
-          var alterText = "<p> Le format n'est pas valide (ex= 10-2015) </p>"
-        } else {
-          var alterText = ""
-        }
-        date.insertAdjacentHTML( 'afterend', alterText );
-
+  dateModules.forEach((dateModule) => {
+    console.log("Là")
+    var dates = dateModule.querySelectorAll('.verifDateWithMonthAndYear')
+    if (dates.length > 0) {
+      dates.forEach((date) => {
+        date.addEventListener('change', function(){
+          var helpMessage = dateModule.querySelector("#helpMessageDate")
+          if(date_regex.test(date.value) || date.value == ""){
+            if(helpMessage){
+              dateModule.removeChild(helpMessage)
+            }
+          } else if(!(date_regex.test(date.value))){
+            var alterText = "<p class='col-xs-12' id='helpMessageDate'> Le format n'est pas valide (ex= 10-2015) </p>"
+            if(!helpMessage){
+              dateModule.insertAdjacentHTML('beforeend', alterText );
+            }
+          }
+        })
       })
-    })
-  }
-  let testDate = (event) => {
-  }
+    }
+  })
 }
 
+if(document.getElementById("add_experience")){
+  document.getElementById("add_experience").addEventListener("click", function(){
+    setTimeout(verifDateWithMonthAndYear, 1000)
+  })
+}
 
 export { verifDateWithMonthAndYear }
