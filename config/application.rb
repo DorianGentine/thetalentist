f4a464f22014b5589aae82aece83e4ee588e33da
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2019-01-07 22:16:30 +0700

    try solution to push heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module TheTalentist
     # pour Ajax
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |generate|
           generate.assets false
           generate.helper false
@@ -32,4 +34,3 @@ module TheTalentist
     # the framework and any gems in your application.
   end
 end
-
