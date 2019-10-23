185f1a9b73cf81ac1f25774611be93c6c7ca91bb
Donatien Rolland <donatien@rollandmail.com>
2019-10-23 21:25:37 +0200

    change maps

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,24 +1,26 @@
-import GMaps from 'gmaps/gmaps.js';
+// A supprimer si après le 01/2020 toujours pas utilisé
 
-const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  mapElement.style.height = mapElement.offsetWidth + "px"
+// import GMaps from 'gmaps/gmaps.js';
 
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(12);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    // map.fitLatLngBounds(markers);
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+// const mapElement = document.getElementById('map');
+// if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  // mapElement.style.height = mapElement.offsetWidth + "px"
 
-  }
-}
+  // const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  // const markers = JSON.parse(mapElement.dataset.markers);
+  // map.addMarkers(markers);
+  // if (markers.length === 0) {
+  //   map.setZoom(12);
+  // } else if (markers.length === 1) {
+  //   map.setCenter(markers[0].lat, markers[0].lng);
+  //   map.setZoom(14);
+  // } else {
+  //   // map.fitLatLngBounds(markers);
+  //   map.setCenter(markers[0].lat, markers[0].lng);
+  //   map.setZoom(14);
+
+  // }
+// }
 
 
 
