a471b8d8293c31bafc1078f42c032dd8074dae08
Donatien Rolland <donatien@rollandmail.com>
2019-11-18 18:57:09 +0100

    new api page

--- app/controllers/api/v1/talents_controller.rb
+++ app/controllers/api/v1/talents_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::TalentsController < Api::V1::BaseController
-  before_action :autorize_call, only: [:repertoire, :analytics]
+  before_action :autorize_call, only: [:repertoire, :analytics, :show]
 
   def repertoire
     talents = Talent.where(:visible => true).reorder(completing: :desc, last_sign_in_at: :desc)
-    @talents = TalentFormat.new(talents).for_repository
+    @talents = TalentFormat.new.for_repository(talents)
   end
 
   def analytics
+  end
+
+  def index
+    @talents = policy_scope(Talent)
+  end
+
+  def show
+    talent = Talent.find(params[:id])
+    @talent = TalentFormat.new.talent(talent)
   end
 
   private
