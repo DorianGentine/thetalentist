8e94c58f7fd78b0ce85cb6a063a9260bdd1b334b
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2019-11-19 12:52:41 +0100

    undo commit 444 from dona

--- app/controllers/api/v1/talents_controller.rb
+++ app/controllers/api/v1/talents_controller.rb
@@ -1,22 +1,24 @@
 class Api::V1::TalentsController < Api::V1::BaseController
-  before_action :autorize_call, only: [:repertoire, :analytics, :show]
+  before_action :autorize_call, only: [:repertoire, :analytics]
+  # before_action :autorize_call, only: [:repertoire, :analytics, :show]
 
   def repertoire
     talents = Talent.where(:visible => true).reorder(completing: :desc, last_sign_in_at: :desc)
-    @talents = TalentFormat.new.for_repository(talents)
+    @talents = TalentFormat.new(talents).for_repository
+    # @talents = TalentFormat.new.for_repository(talents)
   end
 
   def analytics
   end
 
-  def index
-    @talents = policy_scope(Talent)
-  end
+  # def index
+  #   @talents = policy_scope(Talent)
+  # end
 
-  def show
-    talent = Talent.find(params[:id])
-    @talent = TalentFormat.new.talent(talent)
-  end
+  # def show
+  #   talent = Talent.find(params[:id])
+  #   @talent = TalentFormat.new.talent(talent)
+  # end
 
   private
 
