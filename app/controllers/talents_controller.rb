558c90e399e7d586d80a3150151cfd1b4fb9e1c5
Donatien Rolland <donatien@rollandmail.com>
2020-01-21 21:10:41 +0100

    add some commted

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -101,13 +101,19 @@ class TalentsController < ApplicationController
 
   def validation
     @talentist = current_talentist
+    p "YOU ARE IN VALIDATION FONCTION"
     if params[:commit] == "Accepter" && !@talent.validated
       @talent.validated_action(true)
       @talent.set_conversation_between(@talentist)
     elsif params[:commit] == "Refuser"
+      p "YOU ARE IN Refused SECTION"
       if @talent.validated || @talent.validated.nil?
-        @talent.update(declined_params)
+        p "YOU ARE IN UPDATE SECTION TO REFUSED"
+        # @talent.update(declined_params)
+        @talent.update_attributes(talent_params)
+        p "YOU PASS THE UPDATE"
         @talent.validated_action(false)
+        p "YOU PASS VALIDATION FONCTION"
         # @talent.send_refused
       end
     end
@@ -190,6 +196,7 @@ private
       :btoc,
       :btob,
       :photo,
+      :declined,
       :photo_cache,
       :remove_photo,
       :terms_of_condition,
