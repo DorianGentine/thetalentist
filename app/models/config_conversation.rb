2a65018a2c818306209e5980bfe70c61e4f2b791
Donatien Rolland <donatien@rollandmail.com>
2020-03-25 11:15:50 +0100

    add files to config conversation

--- app/models/config_conversation.rb
+++ app/models/config_conversation.rb
@@ -1,4 +1,6 @@
 class ConfigConversation < ApplicationRecord
+  # mount_uploaders :files, PdfUploader
+  has_many_attached :files
   validates :user_email, uniqueness: { scope: [:user_id, :conversation_id]}
   before_create :set_archived_pin
 
