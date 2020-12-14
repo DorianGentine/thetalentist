f8fab4dc01f40c3eff0e0cbfb7f01543f0704f4c
Aleksandr <Aleksandr.Bazilchuk@noveogroup.com>
2020-12-14 11:58:23 +0300

    fix test running

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,16 +1,4 @@
 class ContactFormMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_form_mailer.contact_form.subject
-  #
-  def contact_form
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
-
   def contact_form(contact)
     @email = contact.email
     @name = contact.name
